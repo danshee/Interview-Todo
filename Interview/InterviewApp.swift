ddaa248cc10aba98a8e7d1c65b164d874ba2bb19
danshee <dan.pietsch@gmail.com>
2025-04-17 15:26:03 -0700

    Initial Commit

---
+++ Interview/InterviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  InterviewApp.swift
+//  Interview
+//
+//  Created by Daniel Pietsch on 4/17/25.
+//
+
+import SwiftUI
+
+@main
+struct InterviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
